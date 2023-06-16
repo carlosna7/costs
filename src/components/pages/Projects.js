e082e0288f4883c6a0d4cdc6763e621ef63caccc
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-06-16 16:07:26 -0300

    att

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,30 @@
+import { useLocation } from "react-router-dom"
+
+import { Message } from "../layout/Message"
+import { Container } from "../layout/Container"
+import { LinkButton } from "../layout/LinkButton"
+
+import styles from "./Projects.module.css"
+
 export function Projects() {
 
+    const location = useLocation()
+    let message = ""
+    if (location.state) {
+        message = location.state.message
+    }
+
     return (
-        <h1>Projects</h1>
+        <div>
+            <div>
+                <h1>Meus projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClass="start" >
+        <p>Projetos...</p>
+            </Container>
+        </div>
     )
 
 }
